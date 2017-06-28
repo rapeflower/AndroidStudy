b0b2193a020f5c93f1f163b77fe5c3cd750fd03a
lilei <lilei@j1.com>
2017-06-28 14:17:02 +0800

    lilei

--- app/src/main/java/com/android/lily/view/special/J1ImageView.java
+++ app/src/main/java/com/android/lily/view/special/J1ImageView.java
@@ -21,19 +21,22 @@ import com.android.lily.utils.DisplayUnitUtils;
 public class J1ImageView extends ImageView {
 
     private static final String TAG = J1ImageView.class.getSimpleName();
+    private static final String NAMESPACE = "http://schemas.android.com/apk/res/android";
     /**
      * ImageView显示中心Log的尺寸标准
      */
-    private static final int STANDARD_SIZE_PX_100 = 100;
-    private static final int STANDARD_SIZE_PX_200 = 200;
-    private static final int STANDARD_SIZE_PX_300 = 300;
-    private static final int STANDARD_SIZE_PX_400 = 400;
+    private static int STANDARD_SIZE_PX_100 = 100;
+    private static int STANDARD_SIZE_PX_200 = 200;
+    private static int STANDARD_SIZE_PX_300 = 300;
+    private static int STANDARD_SIZE_PX_400 = 400;
+    private static int defaultSize = 0;
 
     private Context mContext;
     private Paint mPaint;
     private Bitmap mSrcBitmap;
     private boolean isSetImageResource = false;
-    int [] defAttrs = {android.R.attr.src};
+    int [] sysAttrs = {android.R.attr.src};
+    float density = 0;
     int vWidth = 0;//控件的宽度
     int vHeight = 0;//控件的高度
 
@@ -48,9 +51,15 @@ public class J1ImageView extends ImageView {
     public J1ImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         this.mContext = context;
+        STANDARD_SIZE_PX_100 = mContext.getResources().getDimensionPixelOffset(R.dimen.dimen_100px);
+        STANDARD_SIZE_PX_200 = mContext.getResources().getDimensionPixelOffset(R.dimen.dimen_200px);
+        STANDARD_SIZE_PX_300 = mContext.getResources().getDimensionPixelOffset(R.dimen.dimen_300px);
+        STANDARD_SIZE_PX_400 = mContext.getResources().getDimensionPixelOffset(R.dimen.dimen_400px);
+        defaultSize = mContext.getResources().getDimensionPixelOffset(R.dimen.dimen_100px);
+        density = DisplayUnitUtils.getDisplayScaleDensity(mContext);
 
-        TypedArray ta = context.obtainStyledAttributes(attrs, defAttrs, defStyleAttr, 0);
-        int srcResource = attrs.getAttributeResourceValue("http://schemas.android.com/apk/res/android", "src", 0);
+        TypedArray ta = context.obtainStyledAttributes(attrs, sysAttrs, defStyleAttr, 0);
+        int srcResource = attrs.getAttributeResourceValue(NAMESPACE, "src", 0);
         if (srcResource != 0) {
             mSrcBitmap = BitmapFactory.decodeResource(getResources(), srcResource);
         }
@@ -67,11 +76,37 @@ public class J1ImageView extends ImageView {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        vWidth = MeasureSpec.getSize(widthMeasureSpec);
-        vHeight = MeasureSpec.getSize(heightMeasureSpec);
-//        setMeasuredDimension(width, height);
-        android.util.Log.w(TAG, "width = " + vWidth + ", height = " + vHeight);
-        android.util.Log.w(TAG, "scaleDensity = " + DisplayUnitUtils.getDisplayScaleDensity(mContext));
+        vWidth = measureSize(widthMeasureSpec, defaultSize);
+        vHeight = measureSize(heightMeasureSpec, defaultSize);
+        setMeasuredDimension(vWidth, vHeight);
+        //android.util.Log.w(TAG, "width = " + vWidth + ", height = " + vHeight);
+    }
+
+    /**
+     * 测量大小
+     *
+     * @param measureSpec
+     * @param defaultValue
+     * @return
+     */
+    private int measureSize(int measureSpec, int defaultValue) {
+        //设置一个默认值，就是这个View的默认宽度或高度为defaultValue
+        int result = defaultValue;
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+
+        switch (specMode) {
+            // Mode = UNSPECIFIED,AT_MOST时使用提供的默认大小
+            case MeasureSpec.AT_MOST://相当于我们设置为wrap_content
+            case MeasureSpec.UNSPECIFIED:
+                result = defaultValue;
+                break;
+            case MeasureSpec.EXACTLY:// Mode = EXACTLY时使用测量的大小
+                //相当于我们设置为match_parent或者为一个具体的值
+                result = specSize;
+                break;
+        }
+        return result;
     }
 
     @Override
@@ -93,11 +128,11 @@ public class J1ImageView extends ImageView {
         android.util.Log.w(TAG, " min = " + min);
         if (min >= STANDARD_SIZE_PX_400) {
             drawable = DrawableHelper.getDrawable400(mContext);
-        } else if (min >= STANDARD_SIZE_PX_300 && min < STANDARD_SIZE_PX_300) {
+        } else if (min >= STANDARD_SIZE_PX_300 && min < STANDARD_SIZE_PX_400) {
             drawable = DrawableHelper.getDrawable300(mContext);
-        } else if (min >= STANDARD_SIZE_PX_200 && min < STANDARD_SIZE_PX_200) {
+        } else if (min >= STANDARD_SIZE_PX_200 && min < STANDARD_SIZE_PX_300) {
             drawable = DrawableHelper.getDrawable200(mContext);
-        } else if (min >= STANDARD_SIZE_PX_100 && min < STANDARD_SIZE_PX_100) {
+        } else if (min >= STANDARD_SIZE_PX_100 && min < STANDARD_SIZE_PX_200) {
             drawable = DrawableHelper.getDrawable100(mContext);
         } else {
             drawable = DrawableHelper.getDrawable100(mContext);
