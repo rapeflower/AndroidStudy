80e520ae2c0aecb9873f8c2391c781463c1dc6f6
lilei <lilei@lileideMacBook-Pro.local>
2017-07-12 17:57:12 +0800

    lilei

--- app/src/main/java/com/android/lily/activity/TestActivity.java
+++ app/src/main/java/com/android/lily/activity/TestActivity.java
@@ -9,6 +9,7 @@ import com.android.lily.R;
 import com.android.lily.view.special.J1ImageView;
 import com.android.lily.view.special.RoundImageViewByXfermode;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
 
 import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
@@ -37,10 +38,10 @@ public class TestActivity extends Activity{
                         RoundedCornersTransformation.CornerType.TOP))
                 .into(iv);
 
-//        Glide.with(this)
-//                .load("http://img.j1.cn/upload/pic/brandStreet/1499131077234.jpg")
-//                .bitmapTransform(new RoundedCornersTransformation(this, 10, 0,
-//                        RoundedCornersTransformation.CornerType.TOP))
-//                .into(j1ImageView);
+        Glide.with(this)
+                .load("http://img.j1.cn/upload/pic/brandStreet/1499131077234.jpg")
+                .bitmapTransform(new CenterCrop(this), new RoundedCornersTransformation(this, 10, 0,
+                        RoundedCornersTransformation.CornerType.TOP))
+                .into(j1ImageView);
     }
 }
