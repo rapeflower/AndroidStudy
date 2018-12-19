cf70ba15945d6164055e2ccf9e7636e3f64e990c
lilei <lilei@lileideMacBook-Pro.local>
2018-12-19 23:19:29 +0800

    lilei

--- app/src/main/java/com/android/lily/activity/MainActivity.java
+++ app/src/main/java/com/android/lily/activity/MainActivity.java
@@ -4,7 +4,6 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.view.NestedScrollingParentHelper;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
